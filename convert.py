8564ca00e544aac165865842dada1547268dcdca
Olden-Admin <oldenk1@my.smccd.edu>
2024-02-03 20:43:37 -0500

    this is my first commit

--- convert.py
+++ convert.py
@@ -1,2 +1,11 @@
 # Lab 1
-# by Olden and Matheus
+# Group # 5
+# Authors: Kiyale Olden and Matheus Souza
+# Date: 02/03/2024
+# *** This program converts temperature between Fahrenheit and Celsius units *** document me?
+
+
+temp = input('Enter temperature: ')
+user_choice = int(input('Enter 1 for oC to oF, 2 for oF to oC:'))
+
+if user_choice == 1:
